8bebaa2aa0e21ea095fddcf80341e02efe945ea9
Ayende Rahien <Ayende@ayende.com>
2024-12-30 16:06:05 +0200

    Moving away from using Google Docs integration into raw REST calls Better style detection

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-const { authenticate } = require('@google-cloud/local-auth');
-const { google } = require('googleapis');
+const css = require('css');
 const admZip = require("adm-zip");
 const { default: jssoup } = require('jssoup');
 const MetaWeblog = require('metaweblog-api');
@@ -20,68 +19,63 @@ loadLanguages(['lua', 'powershell', 'typescript', 'csharp',
     'nim', 'reason', 'html', 'css', 'scss', 'less', 'stylus', 'pug', 'handlebars',
     'ejs', 'twig', 'bash', 'sh', 'shell', 'awk', 'vim', 'makefile', 'cmake',]);
 
-const SCOPES = [
-    "https://www.googleapis.com/auth/documents",
-    "https://www.googleapis.com/auth/drive.readonly"
-];
 
-
-const TOKEN_PATH = 'C:/Work/Credentials/token.json';
 const CREDENTIALS_PATH = 'C:/Work/Credentials/credentials.json';
+const DATA_PATH = 'C:/Work/Credentials/post-ids.json';
 
-
-async function loadSavedCredentialsIfExist() {
-    try {
-        const content = await fs.readFile(TOKEN_PATH);
-        const credentials = JSON.parse(content);
-        return google.auth.fromJSON(credentials);
-    } catch (err) {
-        return null;
+async function downloadFile(file, type) {
+    const lastSlash = file.lastIndexOf('/');
+    const url = file.substring(0, lastSlash) + "/export?format=" + type;
+    const response = await fetch(url);
+    let filename = null;
+    if (!response.ok) {
+        let errorBody = '';
+        try {
+            // Try to get the response body as text
+            errorBody = await response.text();
+        } catch (bodyError) {
+            console.error('Failed to read error body:', bodyError);
+        }
+        throw new Error(`HTTP error! status: ${response.status}, body: ${errorBody}`);
     }
+    var contentDisposition = response.headers.get('Content-Disposition');
+    const utf8Part = contentDisposition.match(/filename\*=UTF-8''(.+)/);
+    if (utf8Part && utf8Part[1]) {
+        filename = decodeURIComponent(utf8Part[1]);
+        filename = filename.substring(0, filename.lastIndexOf('.'));
+    }
+    return [response, filename];
 }
 
-async function saveCredentials(client) {
-    const content = await fs.readFile(CREDENTIALS_PATH);
-    const keys = JSON.parse(content);
-    const key = keys.installed || keys.web;
-    const payload = JSON.stringify({
-        type: 'authorized_user',
-        client_id: key.client_id,
-        client_secret: key.client_secret,
-        refresh_token: client.credentials.refresh_token,
-    });
-    await fs.writeFile(TOKEN_PATH, payload);
+function matchesSelector(selector, element) {
+    if (selector.startsWith('#')) {
+        return selector.slice(1) === element.id;
+    }
+    if (selector.startsWith('.')) {
+        element.classes = element.classes || (element.attrs.class || '').split(' ');
+        return element.classes.includes(selector.slice(1));
+    }
+    return selector === element.name;
 }
 
-async function authorize() {
-    let client = await loadSavedCredentialsIfExist();
-    if (client) {
-        return client;
-    }
-    client = await authenticate({
-        scopes: SCOPES,
-        keyfilePath: CREDENTIALS_PATH,
-    });
-    if (client.credentials) {
-        await saveCredentials(client);
-    }
-    return client;
+function findMatchingRules(element, rules) {
+    return rules.filter(rule =>
+        rule.selectors.some(selector =>
+            selector.split(' ').every(s => matchesSelector(s, element))
+        )
+    );
 }
 
 
-async function processFile(auth, fileId) {
-    const drive = google.drive({ version: 'v3', auth: auth });
-    const file = await drive.files.export({
-        fileId: fileId,
-        mimeType: 'application/zip',
-    }, { responseType: 'arraybuffer' });
-    const text = await drive.files.export({
-        fileId: fileId,
-        mimeType: 'text/plain'
-    });
+async function processFile(file) {
+
+    const [zipDownload, _] = await downloadFile(file, 'zip');
+    const [textDownload, fileName] = await downloadFile(file, 'txt');
+    const text = await textDownload.text();
+
 
     let blocks = [];
-    for (const match of text.data.matchAll(/\uEC03(.*?)\uEC02/gs)) {
+    for (const match of text.matchAll(/\uEC03(.*?)\uEC02/gs)) {
         const code = match[1].trim();
         let lang = flourite(code, { shiki: true, noUnknown: true }).language;
         if (Prism.languages.hasOwnProperty(lang) == false) {
@@ -94,12 +88,16 @@ async function processFile(auth, fileId) {
             formattedCode + "</code></pre><hr/>");
     }
 
-    const zip = new admZip(Buffer.from(file.data));
+    const zip = new admZip(Buffer.from(await zipDownload.arrayBuffer()));
     const entries = zip.getEntries();
     var htmlText = entries.find(e => path.extname(e.entryName) == '.html')
         .getData().toString('utf8');
 
     const htmlDoc = new jssoup(htmlText)
+    const style = htmlDoc.find('style');
+
+    let styleRules = css.parse(style.text).stylesheet.rules;
+
     const body = htmlDoc.find('body');
 
     let tags = [];
@@ -160,15 +158,19 @@ async function processFile(auth, fileId) {
     })
 
     var currentElement = body.nextElement;
-    let cleanHTML = cleanHtml(currentElement);
+    let cleanHTML = cleanHtml(currentElement, styleRules);
     while (currentElement.nextSibling !== undefined) {
         currentElement = currentElement.nextSibling;
-        cleanHTML += cleanHtml(currentElement);
+        cleanHTML += cleanHtml(currentElement, styleRules);
     }
 
     cleanHTML += `\r\n<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css" integrity="sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==" crossorigin="anonymous" referrerpolicy="no-referrer" />`;
 
-    return [cleanHTML, postId, tags];
+    if (!postId) {
+        postId = readPostId(file);
+    }
+
+    return [cleanHTML, postId, tags, fileName];
 }
 
 function extractDimensions(cssString) {
@@ -182,7 +184,7 @@ function extractDimensions(cssString) {
     }
 }
 
-function cleanHtml(htmlElement) {
+function cleanHtml(htmlElement, styleRules) {
     if (htmlElement.name == "hr") {
         return "<hr/>";
     }
@@ -198,10 +200,7 @@ function cleanHtml(htmlElement) {
         italics: false,
         styles: ''
     };
-    let newStyles = '';
-    if (htmlElement.attrs.hasOwnProperty('style')) {
-        newStyles = computeNewStyles(htmlElement, modifiers);
-    }
+    let newStyles = computeNewStyles(htmlElement, modifiers, styleRules);
     modifiers['styles'] = newStyles;
     let href;
     let classes;
@@ -228,7 +227,7 @@ function cleanHtml(htmlElement) {
     let contents = htmlElement.contents;
     let newContents = '';
     for (let i = 0; i < contents.length; i++) {
-        newContents += cleanHtml(contents[i]);
+        newContents += cleanHtml(contents[i], styleRules);
     }
     if (newContents === "") {
         return '';
@@ -261,7 +260,36 @@ function computeValidStyles(htmlElement) {
     return new Set(validStyles);
 }
 
-function computeNewStyles(htmlElement, modifiers) {
+function applyStyle(styleType, styleValue, htmlElement, modifiers, validStyles) {
+    let newStyles = '';
+
+    if (styleType == 'margin-left' && htmlElement.name === "p") {
+        modifiers['quote'] = styleValue === '36pt';
+    }
+
+    if (validStyles.has(styleType) === false || styleValue === undefined)
+        return newStyles;
+
+    if (styleType === 'color' && (styleValue !== "#000000" && styleValue !== "#1155cc")) {
+        newStyles += 'color:' + styleValue + ';';
+    }
+    if (styleType === 'font-style' && styleValue === 'italic') {
+        modifiers['italics'] = true;
+    }
+    if (styleType === 'font-weight' && styleValue > 500) {
+        modifiers['bold'] = true;
+    }
+    if ((styleType === 'text-decoration-line' || styleType === 'text-decoration') && styleValue === 'underline') {
+        newStyles += 'text-decoration:underline;';
+    }
+    if (styleType === 'text-align') {
+        newStyles += 'text-align:' + styleValue + ';';
+    }
+
+    return newStyles;
+}
+
+function computeNewStyles(htmlElement, modifiers, styleRules) {
     const validStyles = computeValidStyles(htmlElement);
 
     let newStyles = '';
@@ -271,39 +299,28 @@ function computeNewStyles(htmlElement, modifiers) {
     if (htmlElement.name === "tr") {
         modifiers['bold'] = htmlElement.previousElement.name === "table";
     }
-    for (let style of htmlElement.attrs.style.split(';')) {
-        let parts = style.split(':');
-        if (parts.length !== 2)
-            continue;
-        let styleType = parts[0].trim(), styleValue = parts[1].trim();
-        if (styleType == 'margin-left' && htmlElement.name === "p") {
-            modifiers['quote'] = styleValue === '36pt';
-        }
-
-        if (validStyles.has(styleType) === false || styleValue === undefined)
-            continue;
-
-        if (styleType === 'color' && (styleValue !== "#000000" && styleValue !== "#1155cc")) {
-            newStyles += 'color:' + styleValue + ';';
-        }
-        if (styleType === 'font-style' && styleValue === 'italic') {
-            modifiers['italics'] = true;
-        }
-        if (styleType === 'font-weight' && styleValue > 500) {
-            modifiers['bold'] = true;
-        }
-        if ((styleType === 'text-decoration-line' || styleType === 'text-decoration') && styleValue === 'underline') {
-            newStyles += 'text-decoration:underline;';
-        }
-        if (styleType === 'text-align') {
-            newStyles += 'text-align:' + styleValue + ';';
+    if (htmlElement.attrs.style) {
+        for (let style of htmlElement.attrs.style.split(';')) {
+            let parts = style.split(':');
+            if (parts.length !== 2)
+                continue;
+            let styleType = parts[0].trim(), styleValue = parts[1].trim();
+            applyStyle(styleType, styleValue, htmlElement, modifiers, validStyles);
         }
     }
+    for (let match of findMatchingRules(htmlElement, styleRules)) {
+        for (let declaration of match.declarations) {
+            if (declaration.type === 'declaration') {
+                newStyles += applyStyle(declaration.property, declaration.value, htmlElement, modifiers, validStyles,);
+            }
+        }
+    }
+
     return newStyles;
 }
 
 async function getBlogClient() {
-    const content = await fs.readFile(CREDENTIALS_PATH);
+    const content = fs.readFileSync(CREDENTIALS_PATH);
     const blog = JSON.parse(content).blog;
     const blogApi = new MetaWeblog('https://ayende.com/blog/Services/MetaWeblogAPI.ashx');
     return {
@@ -319,27 +336,36 @@ async function getBlogClient() {
     }
 }
 
+function readPostId(key) {
+    let data;
+    try {
+        data = JSON.parse(fs.readFileSync(DATA_PATH, 'utf8'));
+    } catch (err) {
+        data = {};
+    }
+    return data[key];
+}
+
+function writePostId(key, value) {
+    let data;
+    try {
+        data = JSON.parse(fs.readFileSync(DATA_PATH, 'utf8'));
+    } catch (err) {
+        data = {};
+    }
+    data[key] = value;
+    fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2)); // Pretty print with 2 spaces
+}
+
 (async () => {
 
-    let arg = process.argv[2];
+    const file = process.argv[2];
 
-    try {
-        const url = new URL(arg);
-        const parts = url.pathname.split('/');
-        arg = parts[parts.length - 2];
-    } catch {
-        arg = process.argv[1];
-    }
-
-    const file = arg;
-    const auth = await authorize();
-    const [html, postId, tags] = await processFile(auth, file);
-    const docs = google.docs({ version: 'v1', auth });
-    const doc = await docs.documents.get({ documentId: file });
+    const [html, postId, tags, title] = await processFile(file);
 
     var post = {
         description: html,
-        title: doc.data.title,
+        title: title,
         categories: tags,
     };
 
@@ -350,19 +376,9 @@ async function getBlogClient() {
     }
     else {
         const newPostId = await blogClient.createPost(post);
-        await docs.documents.batchUpdate({
-            documentId: file,
-            requestBody: {
-                requests: [{
-                    insertText: {
-                        text: `PostId: ${newPostId}\n`,
-                        location: { index: 1 }
-                    }
-                }]
-            }
-        });
+        writePostId(file, newPostId);
+        console.log('Created: ', newPostId);
     }
-    console.log('Published: ', doc.data.title);
+    console.log('Published: ', title);
     exec("start http://ayende.com/blog/")
-
 })()
